e81888ab9d5d7ff3c74a733bce3ed6fc839a76e0
hubble459 <geraldd459@gmail.com>
2022-11-23 22:25:19 +0100

    added manga_parser to submodules

--- manga_parser/src/model/chapter.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, Clone)]
-pub struct Chapter {
-    pub url: reqwest::Url,
-    pub title: String,
-    pub number: f32,
-    pub posted: Option<chrono::DateTime<chrono::Utc>>,
-}
--- manga_parser/src/model/mod.rs
+++
@@ -1,9 +0,0 @@
-mod search_manga;
-mod chapter;
-mod manga;
-mod scraper_queries;
-
-pub use chapter::Chapter;
-pub use manga::Manga;
-pub use search_manga::SearchManga;
-pub use scraper_queries::*;
