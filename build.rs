ea3fc97a487604f81187c1271eec287dc374d35f
meep334 <geraldd459@gmail.com>
2023-01-19 01:40:35 +0100

    initial grpc init

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@ use std::io;
 #[cfg(windows)] use winres::WindowsResource;
 
 fn main() -> io::Result<()> {
+    tonic_build::compile_protos("proto/helloworld.proto")?;
+
     #[cfg(windows)] {
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
