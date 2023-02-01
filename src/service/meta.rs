d1e48df5192f9288dc6db323216a1942994d22d9
meep334 <geraldd459@gmail.com>
2023-02-01 23:45:46 +0100

    service for meta data like all genres and searchable hostnames

--- src/service/meta.rs
+++ src/service/meta.rs
@@ -1,9 +1,17 @@
-use migration::Expr;
-use sea_orm::{DatabaseConnection, DeriveColumn, EntityTrait, EnumIter, IdenStatic, QuerySelect};
+use manga_parser::parser::Parser;
+use migration::{Expr, IntoCondition, JoinType};
+use sea_orm::{
+    DatabaseConnection, DeriveColumn, EntityTrait, EnumIter, IdenStatic, QuerySelect,
+    RelationTrait, Select,
+};
 use tonic::{Request, Response, Status};
 
+use super::manga::MANGA_PARSER;
+use crate::interceptor::auth::LoggedInUser;
 use crate::proto::meta_server::{Meta, MetaServer};
-use crate::proto::{Empty, MetaReply};
+use crate::proto::{
+    MetaGenresOption, MetaGenresRequest, MetaHostNamesOption, MetaHostNamesRequest, MetaReply,
+};
 
 #[derive(Debug, Default)]
 pub struct MyMeta {}
@@ -13,39 +21,110 @@ enum QueryAs {
     Strings,
 }
 
+enum MetaOption {
+    Reading,
+    Manga,
+    Online,
+}
+
+async fn get_reply(
+    db: &DatabaseConnection,
+    query: Select<entity::manga::Entity>,
+    logged_in: Option<LoggedInUser>,
+    meta_option: MetaOption,
+) -> Result<MetaReply, Status> {
+    let reply = match meta_option {
+        MetaOption::Reading => {
+            let logged_in =
+                logged_in.ok_or(Status::permission_denied("You have to be logged in!"))?;
+
+            MetaReply {
+                items: query
+                    .join(
+                        JoinType::RightJoin,
+                        entity::reading::Relation::Manga.def().rev().on_condition(
+                            move |_left, right| {
+                                Expr::tbl(right, entity::reading::Column::UserId)
+                                    .eq(logged_in.id)
+                                    .into_condition()
+                            },
+                        ),
+                    )
+                    .into_values::<_, QueryAs>()
+                    .all(db)
+                    .await
+                    .map_err(|e| Status::internal(e.to_string()))?,
+            }
+        }
+        MetaOption::Manga => MetaReply {
+            items: query
+                .into_values::<_, QueryAs>()
+                .all(db)
+                .await
+                .map_err(|e| Status::internal(e.to_string()))?,
+        },
+        MetaOption::Online => MetaReply {
+            items: MANGA_PARSER
+                .hostnames()
+                .iter()
+                .map(|url| url.to_string())
+                .collect(),
+        },
+    };
+
+    Ok(reply)
+}
+
 #[tonic::async_trait]
 impl Meta for MyMeta {
-    async fn all(&self, req: Request<Empty>) -> Result<Response<MetaReply>, Status> {
+    async fn genres(&self, req: Request<MetaGenresRequest>) -> Result<Response<MetaReply>, Status> {
         let db = req.extensions().get::<DatabaseConnection>().unwrap();
+        let logged_in = req.extensions().get::<LoggedInUser>().cloned();
+        let request = req.get_ref();
+        let query = entity::manga::Entity::find().select_only().column_as(
+            Expr::cust("distinct unnest(manga.genres)"),
+            QueryAs::Strings,
+        );
 
-
-        // TODO: Get searchable hostnames from MANGA-PARSER not this
-        let hostnames: Vec<String> = entity::manga::Entity::find()
-            .select_only()
-            .column_as(
-                Expr::cust("distinct (regexp_matches(manga.url, '://([^/]+)'))[1]"),
-                QueryAs::Strings,
+        Ok(Response::new(
+            get_reply(
+                db,
+                query,
+                logged_in,
+                match request.option() {
+                    MetaGenresOption::GenresReading => MetaOption::Reading,
+                    MetaGenresOption::GenresManga => MetaOption::Manga,
+                },
             )
-            .into_values::<_, QueryAs>()
-            .all(db)
-            .await
-            .map_err(|e| Status::internal(e.to_string()))?;
+            .await?,
+        ))
+    }
 
-        let tags: Vec<String> = entity::manga::Entity::find()
-            .select_only()
-            .column_as(
-                Expr::cust("distinct unnest(manga.genres)"),
-                QueryAs::Strings,
+    async fn hostnames(
+        &self,
+        req: Request<MetaHostNamesRequest>,
+    ) -> Result<Response<MetaReply>, Status> {
+        let db = req.extensions().get::<DatabaseConnection>().unwrap();
+        let logged_in = req.extensions().get::<LoggedInUser>().cloned();
+        let request = req.get_ref();
+        let query = entity::manga::Entity::find().select_only().column_as(
+            Expr::cust("distinct (regexp_matches(manga.url, '://([^/]+)'))[1]"),
+            QueryAs::Strings,
+        );
+
+        Ok(Response::new(
+            get_reply(
+                db,
+                query,
+                logged_in,
+                match request.option() {
+                    MetaHostNamesOption::HostNamesReading => MetaOption::Reading,
+                    MetaHostNamesOption::HostNamesManga => MetaOption::Manga,
+                    MetaHostNamesOption::HostNamesOnline => MetaOption::Online,
+                },
             )
-            .into_values::<_, QueryAs>()
-            .all(db)
-            .await
-            .map_err(|e| Status::internal(e.to_string()))?;
-
-        Ok(Response::new(MetaReply {
-            tags,
-            hostnames,
-        }))
+            .await?,
+        ))
     }
 }
 
