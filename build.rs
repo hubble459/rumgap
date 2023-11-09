deb8671406f54936f99f1872d7da2d2ce4465571
meep334 <geraldd459@gmail.com>
2023-11-09 15:22:40 +0100

    build(proto): add experimental 'optional' keyword

--- build.rs
+++ build.rs
@@ -15,6 +15,7 @@ fn main() -> io::Result<()> {
     tonic_build::configure()
         .file_descriptor_set_path(descriptor_path)
         .build_client(false)
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(protos.as_slice(), &["proto"])?;
 
     #[cfg(windows)]
